386711a10e87500b21165808ac21527f01bb4ab1
jerzy szajner <jerzy.szajner@gmail.com>
2024-04-11 11:04:48 +0200

    feat(AmountWidget): add AmountWidget class for dynamic amount input handling
    Introduce the AmountWidget class to manage user interactions with quantity
    inputs. This class enhances input validation and supports dynamic value
    adjustments via increase/decrease buttons. It ensures the amount values are
    within predefined limits and emits an 'updated' event to notify other
    components of changes. The implementation includes methods for element
    selection, value setting with validation, and event initialization for user
    actions.

--- dist/js/script.js
+++ dist/js/script.js
@@ -61,10 +61,10 @@
 
       thisProduct.renderInMenu();
       thisProduct.getElements();
+      thisProduct.initAmountWidget();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
       thisProduct.processOrder();
-
 
       //console.log('new Product:', thisProduct);
     }
@@ -91,6 +91,7 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
 
       console.log();
     }
@@ -186,8 +187,83 @@
         }
       }
 
+      /* multiple price by amount */
+      price *= thisProduct.amountWidget.value;
+      
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
+    }
+
+    initAmountWidget() {
+      const thisProduct = this;
+
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+
+      thisProduct.amountWidgetElem.addEventListener('updated', function (){
+        thisProduct.processOrder();
+      });
+    }
+  }
+
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);
+      thisWidget.initActions();
+
+    }
+
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+      thisWidget.defaultMin = settings.amountWidget.defaultMin;
+      thisWidget.defaultMax = settings.amountWidget.defaultMax;
+    }
+
+    setValue(value) {
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      /* TODO: Add validation */
+      if (newValue >= thisWidget.defaultMin && newValue <= thisWidget.defaultMax) {
+        if (thisWidget.value !== newValue && !isNaN(newValue)) {
+          thisWidget.value = newValue;
+          thisWidget.input.value = thisWidget.value;
+          thisWidget.announce();
+        }
+      }
+    }
+
+    initActions() {
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function () {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
+
+    announce() {
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
     }
   }
 
@@ -216,6 +292,7 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
 
       thisApp.initData();
       thisApp.initMenu();
